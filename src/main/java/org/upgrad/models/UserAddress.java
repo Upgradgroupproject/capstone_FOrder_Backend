0727dfd708465b6c0e7ff536a047dfef49e826a1
kumar137 <krshubham137@gmail.com>
2018-12-08 22:11:24 +0530

    Added address controller

---
+++ src/main/java/org/upgrad/models/UserAddress.java
@@ -0,0 +1,62 @@
+package org.upgrad.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_address")
+public class UserAddress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="type")
+    private String type;
+
+    @Column(name="user_id")
+    private int user_id;
+
+    public UserAddress(String type, int user_id, int address_id) {
+        this.type = type;
+        this.user_id = user_id;
+        this.address_id = address_id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getAddress_id() {
+        return address_id;
+    }
+
+    public void setAddress_id(int address_id) {
+        this.address_id = address_id;
+    }
+
+    @Column(name="address_id")
+    private int address_id;
+
+
+
+}
--- src/main/java/org/upgrad/repositories/AddressRepository.java
+++ src/main/java/org/upgrad/repositories/AddressRepository.java
@@ -1,6 +1,22 @@
 package org.upgrad.repositories;
 
 
-public interface AddressRepository {
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.upgrad.models.Address;
+
+@Repository
+public interface AddressRepository extends CrudRepository<Address,Integer>{
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value="INSERT into address(flat_buil_number,locality,city,zipcode,state_id) values(?1,?2,?3,?4,?5)")
+    void addAddress(String flatBuiltNumber, String locality, String city, String zipcode, int state_id);
+
+    @Query(nativeQuery = true,value = "select COUNT(*) from address")
+    int getAddressId();
 
 }
