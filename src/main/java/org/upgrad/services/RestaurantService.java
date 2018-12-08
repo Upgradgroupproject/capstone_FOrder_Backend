399ec97973fd2cae6136942c4e6313f92573d0f4
Spoorthi S Madhavan <36541492+spoorthimadhavan@users.noreply.github.com>
2018-12-08 21:55:24 +0530

    RestaurantService & RestaurantServiceImpl

--- src/main/java/org/upgrad/services/RestaurantService.java
+++ src/main/java/org/upgrad/services/RestaurantService.java
@@ -1,4 +1,11 @@
 package org.upgrad.services;
 
 public interface RestaurantService {
+
+    List<Restaurant> getAllRestaurant();
+    Restaurant getRestaurantByName(RestaurantName);  
+    List<Restaurant> getAllRestaurantByCategory(String categoryName);
+    Restaurant getRestaurantByRestaurantId(int restaurantId);
+    void updateRestaurantDetails(int restaurantId ,String DetailsBody);
 }
+
--- src/main/java/org/upgrad/services/RestaurantServiceImpl.java
+++ src/main/java/org/upgrad/services/RestaurantServiceImpl.java
@@ -1,4 +1,34 @@
 package org.upgrad.services;
 
 public class RestaurantServiceImpl{
+
+    @Override
+    public List<Restaurant> getAllRestaurant() {
+        return RestaurantRepository.getAllRestaurant();
+    }
+    @Override
+    public Restaurant getRestaurantByName(String RestaurantName) {
+        return RestaurantRepository.getRestaurantByName (RestaurantName);
+    }
+    @Override
+    public List<Restaurant> getAllRestaurantByCategory(String categoryName) {
+        return RestaurantRepository.getAllRestaurantByCategory(categoryName);
+    }
+
+    @Override
+    public Restaurant getRestaurantByRestaurantId(int restaurantId) {
+        return RestaurantRepository.getRestaurantByRestaurantId (restaurantId);
+    }
+    @Override
+    public int checkDetailsEntry(int restaurantId) {
+
+        Long restaurantId = RestaurantRepository.checkDetailsEntry (restaurantId);
+
+        if (restaurantId == null) {
+            return 0;
+        } else
+            return restaurantId.intValue ();
+
+
+    }
 }
