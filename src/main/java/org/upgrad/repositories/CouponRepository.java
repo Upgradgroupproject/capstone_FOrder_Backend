1061c06f0af61e95ec7f8f609923c353986cef16
pwndubey77 <pwndubey77@gmail.com>
2018-12-08 17:09:15 +0530

    order-controller_issues

--- src/main/java/org/upgrad/repositories/CouponRepository.java
+++ src/main/java/org/upgrad/repositories/CouponRepository.java
@@ -1,4 +1,15 @@
 package org.upgrad.repositories;
 
-public interface CouponRepository{
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.upgrad.models.Coupon;
+
+@Repository
+public interface CouponRepository extends CrudRepository <Coupon, String> {
+
+    @Query(nativeQuery = true, value= "SELECT * FROM COUPON WHERE coupon_name= ?1")
+    Coupon findCouponByName(String couponName);
+
+
 }
--- src/main/java/org/upgrad/services/OrderServiceImpl.java
+++ src/main/java/org/upgrad/services/OrderServiceImpl.java
@@ -1,4 +1,73 @@
 package org.upgrad.services;
 
-public class OrderServiceImpl{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.upgrad.models.Coupon;
+import org.upgrad.models.Item;
+import org.upgrad.models.Order;
+import org.upgrad.repositories.*;
+import org.upgrad.requestResponseEntity.ItemQuantity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService{
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    //private AddressRepository addressRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public OrderServiceImpl(CouponRepository couponRepository, OrderRepository orderRepository, ItemRepository itemRepository) {
+        this.couponRepository = couponRepository;
+       // this.addressRepository = addressRepository;
+        this.orderRepository = orderRepository;
+        this.itemRepository = itemRepository;
+    }
+
+    @Override
+    public Coupon getCoupon(String couponName) {
+        return couponRepository.findCouponByName(couponName);
+    }
+
+
+
+    @Override
+    public List<Order> getOrdersByUser(Integer userId) {
+        return orderRepository.findOrdersByUser(userId);
+
+    }
+
+    @Override
+    public Integer addOrderWithPermAddress(Integer addressId, Integer paymentId, Integer userId, ArrayList<ItemQuantity> itemQuantities,
+                                           double bill, Integer couponId, double discount) {
+
+
+        orderRepository.saveOrder(couponId, discount, bill, userId, paymentId,  addressId);
+        Integer orderId = orderRepository.findLatestOrderId();
+
+        for (ItemQuantity itemQuantity: itemQuantities) {
+
+            Item item = itemRepository.findItemById (itemQuantity.getItemId());
+            double totalPrice = item.getPrice() * itemQuantity.getQuantity();
+            orderRepository.saveOrderItem(orderId, itemQuantity.getItemId(), itemQuantity.getQuantity(), totalPrice);
+        }
+
+        return orderId;
+    }
+
+    @Override
+    public Integer addOrder(String flatBuilNo, String locality, String city, String zipCode, Integer stateId, String type,
+                            Integer paymentId, Integer userId, List<ItemQuantity> itemQuantities, double bill, Integer couponId,
+                            double discount) {
+
+        return 1;
+    }
 }
