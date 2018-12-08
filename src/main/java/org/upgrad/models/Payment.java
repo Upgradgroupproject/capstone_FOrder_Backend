bae8b9ddea6af12c0d642593cd93cabcd32a6900
pwndubey77 <pwndubey77@gmail.com>
2018-12-08 13:19:12 +0530

    payment controller

--- src/main/java/org/upgrad/models/Payment.java
+++ src/main/java/org/upgrad/models/Payment.java
@@ -1,5 +1,41 @@
 package org.upgrad.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Payment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public String paymentName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPaymentName() {
+        return paymentName;
+    }
+
+    public void setPaymentName(String paymentName) {
+        this.paymentName = paymentName;
+    }
+
+    public Payment() {
+    }
+
+    public Payment(int id, String paymentName) {
+
+        this.id = id;
+        this.paymentName = paymentName;
+    }
 }
