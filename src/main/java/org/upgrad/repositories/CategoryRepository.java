78d5052f7f3bf87e52e9070c1d3c3c7559ba4e04
Spoorthi S Madhavan <36541492+spoorthimadhavan@users.noreply.github.com>
2018-12-09 15:55:45 +0530

    Add files via upload

--- src/main/java/org/upgrad/repositories/CategoryRepository.java
+++ src/main/java/org/upgrad/repositories/CategoryRepository.java
@@ -1,4 +1,25 @@
 package org.upgrad.repositories;
 
-public interface CategoryRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.upgrad.models.Category; 
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Set;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category,Integer> {
+
+    @Query(nativeQuery = true, value="select * from category")
+    List<Category> getAllCategory();
+
+
+
+    @Query(nativeQuery = true,value="select * from question where user_id=?1")
+    List<Category> getAllCategoryByCategoryName(String categoryName);
 }
